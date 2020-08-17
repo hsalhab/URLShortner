4065f46b30051ac0059791852df015970e8d6e1a
hsalhab <husam_salhab@brown.edu>
2020-08-16 21:57:30 -0400

    added react build to express

---
+++ server/build/precache-manifest.f219abe9fbd5f9f9f5ac77c67b339fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9770f7c0332d77b0cf8d5a0e26db854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4446f13f5d10b07507e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4446f13f5d10b07507e",
+    "url": "/static/js/main.e03fe900.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
