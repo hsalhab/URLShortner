0812376fd95302856453c5ae1c84a0fd81a7edbc
hsalhab <husam_salhab@brown.edu>
2020-08-28 17:28:55 -0400

    added general layout

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -4,15 +4,8 @@ import Home from './pages/Home';
 function App() {
   const styles = {
     App: {
-      backgroundColor: "#EBEBD3",
       minHeight: "100vh",
       fontFamily: "Ubuntu",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "calc(10px + 2vmin)",
-      color: "#3C3C3B"
     }
   };
 
--- client/react-app/src/pages/Home.jsx
+++ client/react-app/src/pages/Home.jsx
@@ -17,8 +17,9 @@ class Home extends Component {
   }
 
   handleSubmit = () => {
-      console.log(this.state.url);
-      // event.preventDefault();
+    const response = `URL is: ${this.state.url}, now send it to the server!`;
+    alert(response);
+    // event.preventDefault();
   }
 
   render() {
@@ -27,39 +28,69 @@ class Home extends Component {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "flex-start"
+        justifyContent: "center",
       },
       TextField: {
-        margin: "3%",
-        width: "50ch",
+        margin: "2%",
+        width: "50%",
       },
-      SubmitButton: {
-        margin: "1%",
-      },
+
       primaryDiv: {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        padding: "10%",
+        padding: "8%",
+        color: "#EAF6FF",
+        backgroundColor: "#232528",
+        width: "100%",
       },
-      
+      secondaryDiv: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "8%",
+        color: "#232528",
+        backgroundColor: "#EAF6FF",
+        width: "100%",
+      },
+      p: {
+        color: "#878E94",
+        textAlign: "center",
+        width: "70%"
+      },
+      h1: {
+        fontSize: "calc(30px + 2vmin)",
+        margin: "1%",
+      }
+
     };
 
     return (
       <div style={styles.Home}>
         <div style={styles.primaryDiv}>
-          <h1>URL Shortner</h1>
+          <h1 style={styles.h1} >URL Shortner</h1>
+          <p style={styles.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          </p>
           <TextField style={styles.TextField} id="outlined-basic" label="Enter your URL" variant="outlined" onChange={this.handleChange} />
           <Button style={styles.SubmitButton} variant="contained" color="primary" onClick={this.handleSubmit}>
-                Shorten!
+            Shorten!
           </Button>
         </div>
-        
+        <div style={styles.secondaryDiv}>
+          <h1 style={styles.h1} >Who Are We?</h1>
+          <p style={styles.p}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </p>
+
+        </div>
+
       </div>
     );
   }
-  
+
 }
 
 export default Home;
