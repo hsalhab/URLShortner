f64284c48847ddbeb215fc9a91b98dbbd264934c
hsalhab <husam_salhab@brown.edu>
2020-08-28 16:14:12 -0400

    added text box

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '50ch',
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <h1>URL Shortner</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Enter your URL" variant="outlined" />
+      </form>
     </div>
   );
 }
