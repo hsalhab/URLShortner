63a9310b93c0f5b77d698ca459d6b0ac070b92a7
hsalhab <husam_salhab@brown.edu>
2020-08-30 02:29:41 -0400

    added shorten endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,29 @@ var path = require('path');
 
 router.use(express.static(path.join('./build')));
 
+global.URLDirectory = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join('./build', 'index.html'));
 });
+
+router.get('/shorten', function(req, res, next) {
+  const shortURL = getRandomString(6);
+  global.URLDirectory[shortURL] = req.query.url;
+  res.json({
+    shortURL: shortURL
+  });
+});
+
+function getRandomString(length) {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 module.exports = router;
