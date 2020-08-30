098ba76147e72bbf4da7a178266972b2c2ad2e4f
hsalhab <husam_salhab@brown.edu>
2020-08-30 01:58:55 -0400

    restrucured routes;removed unnecessary boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+router.use(express.static(path.join('./build')));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join('./build', 'index.html'));
 });
 
 module.exports = router;
