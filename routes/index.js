0954a6a8a5ed88050b25aee568410fcdc520dfb9
hsalhab <husam_salhab@brown.edu>
2020-08-30 03:08:43 -0400

    enables CORS in dev env

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var path = require('path');
 
 router.use(express.static(path.join('./build')));
+// router.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 global.URLDirectory = [];
 
